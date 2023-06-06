e829655351f7162b832b101b52df3ece6b3b7918
EvaSteve <eva@example.com>
2023-06-06 22:53:30 +0200

    Revert "Übungen für Float"
    This reverts commit 42b1d2a9e32dd2e2dec65f5983796ebd349db46d.

--- small-projects/project1.py
+++ small-projects/project1.py
@@ -1,21 +1,13 @@
-# zahl = 5
-# zeichenkette = '5'
-# kommazahl = 1.333
-# wahrheitswert = False
-# liste = [1,2,'3',4,5]
-# print(liste)
-# print(type(liste))
-#
-# i = 0
-#
-# while i < 5:
-#     print(i)
-#     i += 1
+zahl = 5
+zeichenkette = '5'
+kommazahl = 1.333
+wahrheitswert = False
+liste = [1,2,'3',4,5]
+print(liste)
+print(type(liste))
 
+i = 0
 
-nummer = int(input('Gib eine Zahl zwischen 100 und 500 ein: '))
-while nummer < 100 or nummer > 500:
-    print('Falsche Nummer, gib eine korrekte Nummer ein!')
-    nummer = int(input('Gib eine Zahl zwischen 100 und 500 ein: '))
-else:
-    print("Eingegebene Zahl ist korrekt: ", nummer)
+while i < 5:
+    print(i)
+    i += 1
