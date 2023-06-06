42b1d2a9e32dd2e2dec65f5983796ebd349db46d
EvaSteve <eva@example.com>
2023-06-06 22:49:17 +0200

    Übungen für Float

--- small-projects/project1.py
+++ small-projects/project1.py
@@ -1,13 +1,21 @@
-zahl = 5
-zeichenkette = '5'
-kommazahl = 1.333
-wahrheitswert = False
-liste = [1,2,'3',4,5]
-print(liste)
-print(type(liste))
+# zahl = 5
+# zeichenkette = '5'
+# kommazahl = 1.333
+# wahrheitswert = False
+# liste = [1,2,'3',4,5]
+# print(liste)
+# print(type(liste))
+#
+# i = 0
+#
+# while i < 5:
+#     print(i)
+#     i += 1
 
-i = 0
 
-while i < 5:
-    print(i)
-    i += 1
+nummer = int(input('Gib eine Zahl zwischen 100 und 500 ein: '))
+while nummer < 100 or nummer > 500:
+    print('Falsche Nummer, gib eine korrekte Nummer ein!')
+    nummer = int(input('Gib eine Zahl zwischen 100 und 500 ein: '))
+else:
+    print("Eingegebene Zahl ist korrekt: ", nummer)
