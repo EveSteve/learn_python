39fe7bbb68de9388d4c1b0b8f522259867570c94
EvaSteve <eva@example.com>
2023-06-06 22:49:17 +0200

    Übungen für Float

---
+++ small-projects/float.py
@@ -0,0 +1,34 @@
+import math
+
+zahl1 = 1.
+print(zahl1)
+print(type(zahl1))
+
+zahl2 = int(zahl1)
+print(zahl2)
+print(type(zahl2))
+
+print(int(1.2321))
+print(int(1.747))
+print(int(-3.94535))
+print(int(-2.19774))
+
+zahl3 = float(input("Gib eine Zahl ein: "))
+zahl4 = float(input("Gib eine weitere Zahl ein: "))
+print(zahl3 + zahl4)
+
+zahl5 = 0.1 + 0.7
+print(zahl5)
+
+print(.1 + .1 + .1 == .3)
+
+print(round(2.5))
+print(round(1.5))
+
+print(math.floor(1.3))
+print(math.floor(1.7))
+print(math.ceil(1.3))
+print(math.ceil(1.7))
+
+print(math.floor(-1.7))
+print(math.ceil(-1.7))
