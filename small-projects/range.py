8dc23432ca5fa1b427bdd7f60294dea106f9c637
EvaSteve <eva@example.com>
2023-07-29 13:55:00 +0200

    Range-Aufgaben

---
+++ small-projects/range.py
@@ -0,0 +1,29 @@
+# Syntax: range(start, stop, step)
+# Parameter:
+# start: [ optional ] Startwert der Zahlenfolge, Standartwert 0
+# stop: Nächster Wert nach dem Endwert der Zahlenfolge
+# step: [ optional ] Integer-Wert, gibt den Abstand zwischen zwei Zahlen in der Zahlenfolge an, Standardwert 1. Bei
+# negativem Wert wird herunter gezählt
+
+# x = range(0, 6, 1)
+x = range(6)
+for n in x:
+    print(n)
+
+
+# Aufgabe 1: Erzeuge eine Zahlenfolge von den Nummern 3 bis 5, und gebe diese aus:
+
+
+# Aufgabe 2: Erzeuge eine Zahlenfolge von den Nummern 3 bis 19, aber gebe nur ungerade Zahlen aus:
+
+
+# Aufgabe 3: Gebe alle durch 5 teilbaren Zahlen zwischen 5 und 35 aus. Zähle dabei allerdings rückwärts:
+
+
+
+# Aufgabe 4: Verfolgständige folgenden Code, so dass die Liste an Obst ausgegeben wird:
+
+fruits = ["Apfel", "Banane", "Kirsche", "Feige"]
+
+for i in range(TODO):
+    print(TODO)
