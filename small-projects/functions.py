f7ae2b322295acfc79c775684d889bb19d8d80ed
EvaSteve <eva@example.com>
2023-10-14 12:34:13 +0200

    Function-Aufgaben

---
+++ small-projects/functions.py
@@ -0,0 +1,41 @@
+# Aufgabe 1: Schreibe eine einfache Funktion, die den Satz "Hello World" ausgibt
+
+print('Das ist  der Start des Programms.')
+# TODO
+print('Und so geht es weiter.')
+# TODO
+print('Und am Ende noch einmal: ')
+# TODO
+
+
+# Aufgabe 2: Alle wollen sich heute vorstellen. Schreibe eine Funktion, die als Argument einen Vornamen nimmt, und den Text: "Hallo, mein Name ist <Vorname>." ausgibt.
+
+# TODO HIER FUNKTION
+
+# vorstellung("Emil")
+# print("Schön, dich kennen zu lernen!")
+# vorstellung("Katja")
+# print("Was für ein schöner Name!")
+# vorstellung("Tobias")
+# print("Mein Onkel heißt auch so!")
+
+# Aufgabe 3: Schreibe eine Funktion, die zwei Werte nimmt, und die Summe der beiden Werte zurückgibt.
+
+# TODO HIER FUNKTION
+
+# zahl1 = input("Gib bitte die erste Zahl ein: ")
+# zahl2 = input("Gib bitte die zweite Zahl ein: ")
+#
+# print("Die Summe der beiden Zahlen ist: " + TODO)
+
+# Aufgabe 4: Schreibe eine Funktion, die ausgibt, wieviel etwas kostet. Wenn kein Wert für den Preis angegeben wird, soll etwas 10 Euro kosten (kosten=10).
+
+# TODO HIER FUNKTION
+
+# zeige_preis("Auto", 15000)
+# zeige_preis("Apfel", 0.5)
+# zeige_preis("Pizza mit Getränk")
+
+
+
+
