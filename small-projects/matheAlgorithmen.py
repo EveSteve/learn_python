57d34b9c1ea6bea9be587a672149d1ef63d6d411
EvaSteve <eva@example.com>
2023-11-08 13:32:12 +0100

    Mathe Algorithmen

---
+++ small-projects/matheAlgorithmen.py
@@ -0,0 +1,35 @@
+# Aufgabe 1: Schreibe einen Algortihmus, der die Fakultät einer Zahl errechnet (z.B. 5!). Gehe dazu zuerst von einer festen Zahl aus.
+
+ausgangszahl = 5
+
+ergebnis = 1
+# TODO, Tipp: for-Schleife mit Range
+
+print("Die Fakultät von", ausgangszahl, "lautet: ", ergebnis)
+
+
+# Aufgabe 1.1: Lasse nun die Zahl, deren Fakultät berechnet werden soll, durch den Benutzer eingeben.
+
+
+
+# Aufgabe 1.2: Schreibe den Code nun so um, dass die Berechnung der Fakultät in einer Funktion stattfindet.
+
+
+
+# Aufgabe 2: Schreibe einen Algorithmus, der prüft, ob eine Zahl eine Primzahl ist.
+# siehe auch: http://schuljahr.inf-schule.de/2019-20/programmierung/funktional/projekte/primzahlen/primzahltest
+
+# ALGORITHMUS istPrimzahl:
+#   Übergabe: n    # natürliche Zahl
+#   prim = True
+#   k = 2
+#   SOLANGE k < n:
+#       WENN n % k == 0:
+#           prim = False
+#       k = k+1
+#   Rückgabe: prim
+
+# Aufgabe 2.1: Lasse nun die Zahl, die darauf geprüft werden soll, ob sie eine Primzahl ist, durch den Benutzer eingeben.
+
+
+# Aufgabe 2.2: Schreibe den Code nun so um, dass die Prüfung in einer Funktion stattfindet.
