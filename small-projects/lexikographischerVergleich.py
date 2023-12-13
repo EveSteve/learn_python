e5386a67d3e0dd4ff86353b12038584622feb2c5
evaSteve <dev@eva-roddeck.de>
2023-12-13 13:42:18 +0100

    Lexikographischer Vergleich

---
+++ small-projects/lexikographischerVergleich.py
@@ -0,0 +1,28 @@
+str1 = "Mark"
+str2 = "Jack"
+
+print(str1 > str2)
+print(str1 < str2)
+print(str1 == str2)
+print(str1 != str2)
+print(str1 >= str2)
+print(str1 <= str2)
+
+
+# islower()
+
+# def check_len(s1, s2):
+#     a = len(s1)
+#     b = len(s2)
+#     if a > b:
+#         print(s1, " is Longer")
+#     elif a == b:
+#         print("Equal Length")
+#     else:
+#         print(s2, " is Longer")
+
+# Aufgabe 1: Schreibe eine Funktion, die zwei Strings einliest, und angibt, welcher im Alphabet zuerst vorkommt
+
+# Aufgabe 2: Schreibe eine Anwendung, welche eine Liste nimmt, und diese Alphabetisch sortiert. Dafür kann sorted(list) benutzt werden.
+
+
