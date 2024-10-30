332cfa838a0183bddbafb032cba44a990d2d735e
evaSteve <dev@eva-roddeck.de>
2024-10-30 16:33:41 +0100

    Inkremente in c++

---
+++ small-projects/inkremente.cpp
@@ -0,0 +1,48 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    int i = 0;
+    i += 5;
+    cout << "i: " << i << "\n";
+
+    int j = 22;
+    j -= 5;
+    cout << "j: " << j << "\n";
+
+    int k = 2;
+    k *= 8;
+    cout << "k: " << k << "\n";
+
+    int l = 235;
+    l /= 5;
+    cout << "l: " << l << "\n";
+}
+
+
+// Aufgabe 1: Was ist der Output dieses Programms?
+//int main()
+//{
+//    int a = 21;
+//    int c ;
+//    c = a++;
+//    cout << c;
+//    return 0;
+//}
+
+
+// Aufgabe 2: Was ist der Output dieses Programms?
+//int main()
+//{
+//    int x = 5, y = 5;
+//    cout << ++x << --y << endl;
+//    return 0;
+//}
+
+// Aufgabe 3: Schreibe ein Programm, welches 5x die Zahl 0 um 2 erhöht und diese ausgibt, ohne eine Schleife zu benutzen
+
+// Aufgabe 4: Schreibe das Programm aus Aufgabe 3, aber mit einer Schleife
